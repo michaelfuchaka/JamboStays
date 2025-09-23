19d036ae08730c9c9fe70d365c1c73344569dd9b
michaelfuchaka <fuchakamichael06@gmail.com>
2025-09-23 20:45:38 +0300

    refactor: remove unused components and API functions for cleaner codebase

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
