f8c4c342beab29c10964f2ae0f1dea2da9d748e0
michaelfuchaka <fuchakamichael06@gmail.com>
2025-09-27 06:55:16 +0300

    Update API baseURL to production backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5555/api", 
+  baseURL: "https://jambostays-backend-v2.onrender.com/api", 
   headers: {
     'Content-Type': 'application/json',
   },
